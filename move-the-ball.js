2e4c0bd9797b1f6aad397a86f166d7f6579e392d
Onisko <Onisko@MacBook.local>
2019-05-21 17:04:57 +0300

    fixed intendations

--- move-the-ball.js
+++ move-the-ball.js
@@ -4,29 +4,29 @@ const ball = document.getElementById('ball');
 field.addEventListener('click', (event)=> {
   const innerFieldCoords = {
     y: event.clientY - (field.offsetTop + field.clientTop),
-   x: event.clientX - (field.offsetLeft + field.clientLeft),
+    x: event.clientX - (field.offsetLeft + field.clientLeft),
   }
-  console.log(innerFieldCoords);
+
   const ballCoords = {
     x: innerFieldCoords.x - ball.offsetWidth/2,
     y: innerFieldCoords.y - ball.offsetHeight/2
   }
-  console.log(ballCoords);
+
   if (ballCoords.y < 0) {
-   ballCoords.y = 0;
+    ballCoords.y = 0;
   }
 
   if (ballCoords.x < 0) {
-   ballCoords.x = 0;
+    ballCoords.x = 0;
   }
   if (ballCoords.y + ball.clientHeight > field.clientHeight) {
     ballCoords.y = field.clientHeight - ball.clientHeight;
   }
 
   if (ballCoords.x + ball.clientWidth > field.clientWidth) {
-  ballCoords.x = field.clientWidth - ball.clientWidth;
+    ballCoords.x = field.clientWidth - ball.clientWidth;
   }
 
   ball.style.top = `${ballCoords.y}px`;
   ball.style.left = `${ballCoords.x}px`;
-})
+});
